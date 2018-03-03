394b02758ddbb007e45383b0598b30d4925e11d2
blackflux <blackflux@unfine.de>
2018-03-03 14:06:54 -0800

    fix: renamed environment variable used for testing

--- test/handler.js
+++ test/handler.js
@@ -1,11 +1,11 @@
 const api = require("./../lib/api")();
 
-module.exports.json = api.wrap(process.env.RATE_LIMIT_MAX, () => new api.JsonResponse({ some: "json" }));
+module.exports.json = api.wrap(process.env.RATE_LIMIT, () => new api.JsonResponse({ some: "json" }));
 
-module.exports.error = api.wrap(process.env.RATE_LIMIT_MAX, () => {
+module.exports.error = api.wrap(process.env.RATE_LIMIT, () => {
   throw new api.ApiError("Some Error");
 });
 
-module.exports.exception = api.wrap(process.env.RATE_LIMIT_MAX, () => {
+module.exports.exception = api.wrap(process.env.RATE_LIMIT, () => {
   throw new Error("Some Exception");
 });
