f33728647612c802a478daacc6121c1821abb12d
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-03-06 16:32:43 -0800

    feat: now validating directly on export

--- test/test_swagger.js
+++ test/test_swagger.js
@@ -7,7 +7,9 @@ const generateSwagger = require("./handler").generateSwagger;
 describe("Testing Swagger", () => {
   it("Testing Generation", (done) => {
     const file = path.join(appRoot.path, `swagger.json`);
-    fs.writeFileSync(file, JSON.stringify(generateSwagger(), null, 2));
-    SwaggerParser.validate(file, err => done(err));
+    generateSwagger().then((api) => {
+      fs.writeFileSync(file, JSON.stringify(api, null, 2));
+      SwaggerParser.validate(file, err => done(err));
+    });
   });
 });
