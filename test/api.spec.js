8fc18115765578eb7bce2757a9f2c61ca7507d94
jeffsoloshy@gmail.com <eb4cdf0b742fed712b6f983a36b19896a9735da9>
2018-09-25 13:40:32 -0700

    feat: added test to ensure exports match

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const difference = require('lodash.difference');
+const expect = require('chai').expect;
 const lambdaTester = require("lambda-tdd")({
   cwd: path.join(__dirname, ".."),
   verbose: process.argv.slice(2).indexOf("--verbose") !== -1,
@@ -7,5 +9,16 @@ const lambdaTester = require("lambda-tdd")({
   envVarYml: path.join(__dirname, "env.yml"),
   testFolder: path.join(__dirname, "handler")
 });
+const api = require('../src/api');
 
 lambdaTester.execute();
+
+it("Testing Exports Synchronized.", () => {
+  expect(difference(Object.keys(api), Object.keys(api.Api()))).to.deep.equal(["Api"]);
+  expect(difference(Object.keys(api.Api()), Object.keys(api)).sort()).to.deep.equal([
+    "generateDifference",
+    "generateSwagger",
+    "rollbar",
+    "wrap"
+  ]);
+});
