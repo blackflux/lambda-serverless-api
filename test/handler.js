0ffdec66769f9348f7e0bf53286e174496b31ac0
Lukas Siemon <github@blackflux.com>
2018-11-06 14:48:17 -0800

    fix: updated deps

--- test/handler.js
+++ test/handler.js
@@ -1,52 +1,52 @@
-const api = require("./../src/api").Api();
+const api = require('./../src/api').Api();
 
-module.exports.error = api.wrap("GET error", [], process.env.RATE_LIMIT, () => {
-  throw api.ApiError("Some Error", 400, 2341);
+module.exports.error = api.wrap('GET error', [], process.env.RATE_LIMIT, () => {
+  throw api.ApiError('Some Error', 400, 2341);
 });
 
-module.exports.exception = api.wrap("GET exception", [], process.env.RATE_LIMIT, () => {
-  throw Error("Some Exception");
+module.exports.exception = api.wrap('GET exception', [], process.env.RATE_LIMIT, () => {
+  throw Error('Some Exception');
 });
 
-module.exports.text = api.wrap("GET text", [], process.env.RATE_LIMIT, () => api.ApiResponse("some text"));
+module.exports.text = api.wrap('GET text', [], process.env.RATE_LIMIT, () => api.ApiResponse('some text'));
 
-module.exports.json = api.wrap("GET json", [], process.env.RATE_LIMIT, () => api.JsonResponse({ some: "json" }));
+module.exports.json = api.wrap('GET json', [], process.env.RATE_LIMIT, () => api.JsonResponse({ some: 'json' }));
 
-module.exports.proxy = api.wrap("GET proxy/{proxy+}", [
-  api.Str("proxy+", "path")
+module.exports.proxy = api.wrap('GET proxy/{proxy+}', [
+  api.Str('proxy+', 'path')
 ], process.env.RATE_LIMIT, ({ proxy }) => api.JsonResponse({ path: proxy }));
 
-module.exports.param = api.wrap("POST param", [
-  api.Str("username", "json"),
-  api.Email("email", "json", false),
-  api.Str("ref", "query", false),
-  api.RegEx("notification", "^(1|0)$", "query", false),
-  api.Str("authorizer", "context", false),
-  api.Str("authorizer", "header", false),
-  api.UUID("uuidParam", "json", false),
-  api.List("listParam", "json", false),
-  api.StrList("strListParam", "json", false),
-  api.FieldsParam("fieldsParam", "id", "json", false),
-  api.NumberList("numberListParam", "json", false),
-  api.GeoPoint("geoPointParam", "json", false),
-  api.GeoRect("geoRectParam", "json", false),
-  api.List("listParam", "query", false),
-  api.StrList("strListParam", "query", false),
-  api.FieldsParam("fieldsParam", "id", "query", false),
-  api.NumberList("numberListParam", "query", false),
-  api.GeoPoint("geoPointParam", "query", false),
-  api.GeoRect("geoRectParam", "query", false),
-  api.Json("jsonParam", api.Joi.object().required(), "json", false),
-  api.Json("jsonParam", api.Joi.object().required(), "query", false)
+module.exports.param = api.wrap('POST param', [
+  api.Str('username', 'json'),
+  api.Email('email', 'json', false),
+  api.Str('ref', 'query', false),
+  api.RegEx('notification', '^(1|0)$', 'query', false),
+  api.Str('authorizer', 'context', false),
+  api.Str('authorizer', 'header', false),
+  api.UUID('uuidParam', 'json', false),
+  api.List('listParam', 'json', false),
+  api.StrList('strListParam', 'json', false),
+  api.FieldsParam('fieldsParam', 'id', 'json', false),
+  api.NumberList('numberListParam', 'json', false),
+  api.GeoPoint('geoPointParam', 'json', false),
+  api.GeoRect('geoRectParam', 'json', false),
+  api.List('listParam', 'query', false),
+  api.StrList('strListParam', 'query', false),
+  api.FieldsParam('fieldsParam', 'id', 'query', false),
+  api.NumberList('numberListParam', 'query', false),
+  api.GeoPoint('geoPointParam', 'query', false),
+  api.GeoRect('geoRectParam', 'query', false),
+  api.Json('jsonParam', api.Joi.object().required(), 'json', false),
+  api.Json('jsonParam', api.Joi.object().required(), 'query', false)
 ], process.env.RATE_LIMIT, params => api.JsonResponse(params));
 
-module.exports.param2 = api.wrap("POST param2", [
-  api.Str("username", "json", false),
-  api.Email("email", "json", false, { nullable: true }),
-  api.Str('X-Custom-Header', "header", false)
+module.exports.param2 = api.wrap('POST param2', [
+  api.Str('username', 'json', false),
+  api.Email('email', 'json', false, { nullable: true }),
+  api.Str('X-Custom-Header', 'header', false)
 ], process.env.RATE_LIMIT, ({
-  username = "default",
-  email = "default",
+  username = 'default',
+  email = 'default',
   xCustomHeader = null
 }) => api.JsonResponse({ username, email, xCustomHeader }));
 
--- test/swagger.spec.js
+++ test/swagger.spec.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const expect = require('chai').expect;
 const yaml = require('js-yaml');
 const appRoot = require('app-root-path');
-const api = require("./handler").internalApi;
+const api = require('./handler').internalApi;
 
-describe("Testing Swagger", () => {
-  it("Updating Swagger File with API definitions.", (done) => {
-    const file = path.join(appRoot.path, "test", "resources", `swagger.yml`);
+describe('Testing Swagger', () => {
+  it('Updating Swagger File with API definitions.', (done) => {
+    const file = path.join(appRoot.path, 'test', 'resources', 'swagger.yml');
     Promise.resolve(fs.readFileSync(file))
       .then(yaml.safeLoad)
       .then(api.generateSwagger)
@@ -17,14 +17,14 @@ describe("Testing Swagger", () => {
       .catch(done.fail);
   });
 
-  it("Testing serverless.yml", () => {
+  it('Testing serverless.yml', () => {
     expect(api.generateDifference(
-      path.join(appRoot.path, "test", "resources", `swagger.yml`),
-      path.join(appRoot.path, "test", "resources", `serverless.yml`)
+      path.join(appRoot.path, 'test', 'resources', 'swagger.yml'),
+      path.join(appRoot.path, 'test', 'resources', 'serverless.yml')
     )).to.deep.equal([]);
   });
 
-  it("Testing Empty Compare", () => {
-    expect(JSON.stringify(api.generateSwagger())).to.equal("{}");
+  it('Testing Empty Compare', () => {
+    expect(JSON.stringify(api.generateSwagger())).to.equal('{}');
   });
 });
