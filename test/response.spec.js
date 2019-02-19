8833f47085665273254ba419bf0969b011f4ecaa
Lukas Siemon <github@blackflux.com>
2019-02-19 13:25:48 -0800

    amend: simplified test

--- test/response.spec.js
+++ test/response.spec.js
@@ -16,17 +16,18 @@ describe('Testing Response', () => {
   });
 
   it('Testing Multi Methods for Options Request', (done) => {
-    let allowedMethodsPrev = null;
-    api = Api({
-      preflightCheck: ({ allowedMethods }) => {
-        allowedMethodsPrev = allowedMethods;
-        return false;
-      }
-    });
+    api = Api({ preflightCheck: args => args });
     api.wrap('GET path', [], 10);
     api.wrap('DELETE path', [], 10);
-    api.router({ httpMethod: 'OPTIONS', path: '/path' }, {}, () => {
-      expect(allowedMethodsPrev).to.deep.equal(['OPTIONS', 'GET', 'DELETE']);
+    api.router({ httpMethod: 'OPTIONS', path: '/path' }, {}, (err, resp) => {
+      expect(err).to.equal(null);
+      expect(resp).to.deep.equal({
+        statusCode: 200,
+        headers: {
+          path: 'path',
+          allowedMethods: ['OPTIONS', 'GET', 'DELETE']
+        }
+      });
       done();
     });
   });
