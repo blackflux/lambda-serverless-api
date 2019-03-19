d86baaf2b8347769a252bd85c375f97f979fbb69
Lukas Siemon <github@blackflux.com>
2019-03-19 11:40:22 -0700

    chore: separated geopoint parameter tests

--- test/param.spec.js
+++ test/param.spec.js
@@ -29,34 +29,6 @@ describe('Testing Params', () => {
     done();
   });
 
-  it('Testing GeoPoint Parameter (query)', () => {
-    const param = api.GeoPoint('geoPoint');
-    expect(param.get({
-      queryStringParameters: {
-        geoPoint: '[-119.491,49.892]'
-      }
-    })).to.deep.equal([-119.491, 49.892]);
-    ['[-181,0]', '[181,0]', '[0,-91]', '[0,91]', '[0,0,0]'].forEach((geoPoint) => {
-      expect(() => param.get({
-        queryStringParameters: { geoPoint }
-      }), `GeoPoint: ${geoPoint}`).to.throw('Invalid Value for query-Parameter "geoPoint" provided.');
-    });
-  });
-
-  it('Testing GeoPoint Parameter (json)', () => {
-    const param = api.GeoPoint('geoPoint', 'json');
-    expect(param.get({
-      body: {
-        geoPoint: [-119.491, 49.892]
-      }
-    })).to.deep.equal([-119.491, 49.892]);
-    [[-181, 0], [181, 0], [0, -91], [0, 91], [0, 0, 0], '0,0'].forEach((geoPoint) => {
-      expect(() => param.get({
-        body: { geoPoint }
-      }), `GeoPoint: ${geoPoint}`).to.throw('Invalid Value for json-Parameter "geoPoint" provided.');
-    });
-  });
-
   it('Testing GeoRect Parameter (query)', () => {
     const param = api.GeoRect('geoRect');
     expect(param.get({
---
+++ test/params/geo-point.spec.js
@@ -0,0 +1,39 @@
+const expect = require('chai').expect;
+const api = require('../../src/api').Api();
+
+describe('Testing GeoPoint Parameter', () => {
+  const queryParam = api.GeoPoint('geoPoint');
+  const jsonParam = api.GeoPoint('geoPoint', 'json');
+
+  it('Testing valid query parameter', () => {
+    expect(queryParam.get({
+      queryStringParameters: {
+        geoPoint: '[-119.491,49.892]'
+      }
+    })).to.deep.equal([-119.491, 49.892]);
+  });
+
+  it('Testing invalid query parameter', () => {
+    ['[-181,0]', '[181,0]', '[0,-91]', '[0,91]', '[0,0,0]'].forEach((geoPoint) => {
+      expect(() => queryParam.get({
+        queryStringParameters: { geoPoint }
+      }), `GeoPoint: ${geoPoint}`).to.throw('Invalid Value for query-Parameter "geoPoint" provided.');
+    });
+  });
+
+  it('Testing valid json parameter', () => {
+    expect(jsonParam.get({
+      body: {
+        geoPoint: [-119.491, 49.892]
+      }
+    })).to.deep.equal([-119.491, 49.892]);
+  });
+
+  it('Testing invalid json parameter', () => {
+    [[-181, 0], [181, 0], [0, -91], [0, 91], [0, 0, 0], '0,0'].forEach((geoPoint) => {
+      expect(() => jsonParam.get({
+        body: { geoPoint }
+      }), `GeoPoint: ${geoPoint}`).to.throw('Invalid Value for json-Parameter "geoPoint" provided.');
+    });
+  });
+});
