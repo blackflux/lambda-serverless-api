c51abe808569cbc648193f1dd46e725c2952ddf4
Lukas Siemon <github@blackflux.com>
2019-03-19 10:52:12 -0700

    chore: separated uuid parameter tests

---
+++ test/params/uuid.spec.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect;
+const api = require('../../src/api').Api();
+
+describe('Testing UUID Parameter', () => {
+  const queryParam = api.UUID('value');
+
+  it('Testing UUID valid query parameter', () => {
+    expect(queryParam.get({
+      queryStringParameters: {
+        value: '9d09d573-173e-4919-b823-70d406242040'
+      }
+    })).to.equal('9d09d573-173e-4919-b823-70d406242040');
+  });
+
+  it('Testing UUID invalid query parameter', () => {
+    expect(() => queryParam.get({
+      queryStringParameters: {
+        value: 'invalid'
+      }
+    })).to.throw('Invalid Value for query-Parameter "value" provided.');
+  });
+});
