c52dd5eb3ee5972b1c2d73e8b275d904d4a2a79b
Lukas Siemon <github@blackflux.com>
2019-03-19 11:26:42 -0700

    chore: separated isodate parameter tests

--- test/param.spec.js
+++ test/param.spec.js
@@ -29,51 +29,6 @@ describe('Testing Params', () => {
     done();
   });
 
-  it('Testing IsoDate Parameter (query)', () => {
-    const param = api.IsoDate('value');
-    expect(param.get({
-      queryStringParameters: {
-        value: '2008-09-15T15:53:00+05:00'
-      }
-    })).to.equal('2008-09-15T15:53:00+05:00');
-    expect(() => param.get({
-      queryStringParameters: {
-        value: '2009-02-30T15:53:00+05:00'
-      }
-    })).to.throw('Invalid Value for query-Parameter "value" provided.');
-    expect(() => param.get({
-      queryStringParameters: {
-        value: '2008-09-15T15:53:00'
-      }
-    })).to.throw('Invalid Value for query-Parameter "value" provided.');
-  });
-
-  it('Testing IsoDate Parameter (json)', () => {
-    const param = api.IsoDate('value', 'json');
-    expect(param.get({
-      body: {
-        value: '2008-09-15T15:53:00+05:00'
-      }
-    })).to.equal('2008-09-15T15:53:00+05:00');
-    expect(() => param.get({
-      body: {
-        value: '2009-02-30T15:53:00+05:00'
-      }
-    })).to.throw('Invalid Value for json-Parameter "value" provided.');
-    expect(() => param.get({
-      body: {
-        value: '2008-09-15T15:53:00'
-      }
-    })).to.throw('Invalid Value for json-Parameter "value" provided.');
-  });
-
-  it('Testing IsoDate Parameter Undefined (json)', () => {
-    const param = api.IsoDate('value', 'json', false);
-    expect(param.get({
-      body: {}
-    })).to.equal(undefined);
-  });
-
   it('Testing StrList Parameter (query)', () => {
     const param = api.StrList('list');
     expect(param.get({
