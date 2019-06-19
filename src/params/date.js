77b57675b014e42e3e86a21950b9e136a82f080d
Kodi Ackerman <kodiackerman@msn.com>
2019-06-19 15:10:45 -0700

    amend: Removed capture groups

--- src/params/date.js
+++ src/params/date.js
@@ -4,7 +4,7 @@ const RegEx = require('./regex');
 class Date extends RegEx {
   constructor(name, position, opts) {
     super(name, position, Object.assign({}, opts, {
-      regex: new RegExp(/^(\d{4})-(\d{2})-(\d{2})$/)
+      regex: new RegExp(/^\d{4}-\d{2}-\d{2}$/)
     }));
   }
 
