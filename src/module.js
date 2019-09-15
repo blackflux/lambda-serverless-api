e5b3b06f2b1edd2d4e338ef5bb3873925b9c5166
Lukas Siemon <lukas.siemon@gmail.com>
2019-09-15 11:04:57 -0700

    amend: missing file

--- src/module.js
+++ src/module.js
@@ -1,14 +1,25 @@
+const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const get = require('lodash.get');
 
 class Module {
   constructor(pluginPath, options) {
-    this.plugins = fs
+    const plugins = fs
       .readdirSync(pluginPath)
       // eslint-disable-next-line import/no-dynamic-require,global-require
       .map((plugin) => require(path.join(pluginPath, plugin)))
-      .map((P) => new P(get(options, P.getOptionPath(), {})));
+      .map((plugin) => [plugin, plugin.schema()]);
+    this.schemas = plugins.map(([_, schema]) => schema);
+    this.plugins = plugins.map(([P, schema]) => {
+      const schemaPath = Object.keys(schema);
+      assert(schemaPath.length === 1);
+      return new P(get(options, schemaPath[0], {}));
+    });
+  }
+
+  getSchemas() {
+    return this.schemas;
   }
 
   before({ event }) {
