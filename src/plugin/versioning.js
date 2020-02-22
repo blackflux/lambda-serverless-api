dce6f1a21cac51427cc9dcc3c4b89a41160b56f5
Lukas Siemon <lukas.siemon@gmail.com>
2020-02-22 08:59:14 -0800

    amend: more refactoring

--- src/plugin/versioning.js
+++ src/plugin/versioning.js
@@ -7,8 +7,7 @@ const { Plugin } = require('../plugin');
 const { ApiError } = require('../response');
 
 const VERSION_REGEX = /^\d+\.\d+\.\d+$/;
-
-const { deprecationHeaderRegexp, sunsetHeaderRegexp } = (() => {
+const HEADER_REGEX = (() => {
   const dateFormat = [
     '(Sun|Mon|Tue|Wed|Thu|Fri|Sat), ',
     '([1-9]?0[1-9]|[1-2]?[0-9]|3[01]) ',
@@ -17,55 +16,94 @@ const { deprecationHeaderRegexp, sunsetHeaderRegexp } = (() => {
     '(2[0-3]|[0-1][0-9]):([0-5][0-9])(?::(60|[0-5][0-9])) ',
     '([-\\+][0-9]{2}[0-5][0-9]|(?:UT|GMT|(?:E|C|M|P)(?:ST|DT)|[A-IK-Z]))'
   ];
-
   return {
-    deprecationHeaderRegexp: new RegExp(['^date="', ...dateFormat, '"$'].join('')),
-    sunsetHeaderRegexp: new RegExp(['^', ...dateFormat, '$'].join(''))
+    deprecation: new RegExp(['^date="', ...dateFormat, '"$'].join('')),
+    sunset: new RegExp(['^', ...dateFormat, '$'].join(''))
   };
 })();
+
+const Executor = ({ apiVersionHeader, sunsetDurationInDays, versions: versionsRaw }) => {
+  const versions = Object.entries(versionsRaw)
+    .map(([version, date]) => ({ version, date, unix: Date.parse(date) }))
+    .sort((a, b) => (pv.test(`${a.version} < ${b.version}`) ? -1 : 1))
+    .map((e, idx, arr) => Object.assign(e, {
+      isDeprecated: arr[idx + 1] !== undefined,
+      deprecationDate: arr[idx + 1] !== undefined ? new Date(arr[idx + 1].unix) : null,
+      sunsetDate: arr[idx + 1] !== undefined
+        ? new Date(arr[idx + 1].unix + sunsetDurationInDays * 1000 * 60 * 60 * 24)
+        : null
+    }))
+    .reduce((p, c) => Object.assign(p, { [c.version]: c }), {});
+
+  const getApiVersionMeta = ({ headers, httpMethod }) => {
+    if (httpMethod === 'OPTIONS') {
+      return null;
+    }
+    if (apiVersionHeader === undefined) {
+      return null;
+    }
+    const apiVersion = headers[apiVersionHeader.toLowerCase()];
+    if (apiVersion === undefined) {
+      throw ApiError(`Required header "${apiVersionHeader}" missing`, 403);
+    }
+    if (!VERSION_REGEX.test(apiVersion)) {
+      throw ApiError(`Invalid value "${apiVersion}" for header "${apiVersionHeader}" provided`, 403);
+    }
+    if (versions[apiVersion] === undefined) {
+      throw ApiError(`Unknown version "${apiVersion}" for header "${apiVersionHeader}" provided`, 403);
+    }
+    return versions[apiVersion];
+  };
+
+  return {
+    getApiVersionMeta,
+    updateDeprecationHeader: (event, response) => {
+      const versionMeta = getApiVersionMeta(event);
+      if (versionMeta === null) {
+        return;
+      }
+      const { isDeprecated, deprecationDate, sunsetDate } = versionMeta;
+      if (!isDeprecated) {
+        return;
+      }
+
+      ['deprecation', 'sunset'].forEach((header) => {
+        assert(
+          response.headers[header] === undefined || HEADER_REGEX[header].test(response.headers[header]),
+          `Bad format "${response.headers[header]}" for response header "${header}" detected`
+        );
+      });
+      if (
+        response.headers.deprecation === undefined
+        || deprecationDate < Date.parse(response.headers.deprecation.slice(6, -1))
+      ) {
+        set(response.headers, 'deprecation', `date="${deprecationDate.toUTCString()}"`);
+      }
+      if (
+        response.headers.sunset === undefined
+        || sunsetDate < Date.parse(response.headers.sunset)
+      ) {
+        set(response.headers, 'sunset', sunsetDate.toUTCString());
+      }
+    }
+  };
+};
 
 class Versioning extends Plugin {
   constructor(options) {
     super(options);
-    const sunsetDurationInDays = get(options, 'sunsetDurationInDays');
-    const apiVersionHeader = get(options, 'apiVersionHeader');
-    this.versions = Object.entries(get(options, 'versions', {}))
-      .map(([version, date]) => ({ version, date, unix: Date.parse(date) }))
-      .sort((a, b) => (pv.test(`${a.version} < ${b.version}`) ? -1 : 1))
-      .map((e, idx, arr) => Object.assign(e, {
-        isDeprecated: arr[idx + 1] !== undefined,
-        deprecationDate: arr[idx + 1] !== undefined ? new Date(arr[idx + 1].unix) : null,
-        sunsetDate: arr[idx + 1] !== undefined
-          ? new Date(arr[idx + 1].unix + sunsetDurationInDays * 1000 * 60 * 60 * 24)
-          : null
-      }))
-      .reduce((p, c) => Object.assign(p, { [c.version]: c }), {});
-    this.parseApiVersion = ({ headers, httpMethod }) => {
-      if (httpMethod === 'OPTIONS') {
-        return null;
-      }
-      if (apiVersionHeader === undefined) {
-        return null;
-      }
-      const apiVersion = headers[apiVersionHeader.toLowerCase()];
-      if (apiVersion === undefined) {
-        throw ApiError(`Required header "${apiVersionHeader}" missing`, 403);
-      }
-      if (!VERSION_REGEX.test(apiVersion)) {
-        throw ApiError(`Invalid value "${apiVersion}" for header "${apiVersionHeader}" provided`, 403);
-      }
-      if (this.versions[apiVersion] === undefined) {
-        throw ApiError(`Unknown version "${apiVersion}" for header "${apiVersionHeader}" provided`, 403);
-      }
-      return apiVersion;
-    };
+    this.executor = Executor({
+      apiVersionHeader: get(options, 'apiVersionHeader'),
+      sunsetDurationInDays: get(options, 'sunsetDurationInDays'),
+      versions: get(options, 'versions', {})
+    });
   }
 
   static schema() {
     return {
       versioning: Joi.object().keys({
-        sunsetDurationInDays: Joi.number().integer().min(0),
         apiVersionHeader: Joi.string(),
+        sunsetDurationInDays: Joi.number().integer().min(0),
         versions: Joi.object().pattern(
           Joi.string().pattern(VERSION_REGEX),
           Joi.date().iso()
@@ -79,32 +117,11 @@ class Versioning extends Plugin {
   }
 
   async before({ event, response, router }) {
-    this.parseApiVersion(event);
+    this.executor.getApiVersionMeta(event);
   }
 
   async after({ event, response, router }) {
-    const apiVersion = this.parseApiVersion(event);
-    if (apiVersion === null) {
-      return;
-    }
-    assert(response.headers.sunset === undefined || sunsetHeaderRegexp.test(response.headers.sunset));
-    assert(response.headers.deprecation === undefined || deprecationHeaderRegexp.test(response.headers.deprecation));
-    const { isDeprecated, sunsetDate, deprecationDate } = this.versions[apiVersion];
-    if (!isDeprecated) {
-      return;
-    }
-    if (
-      response.headers.sunset === undefined
-      || sunsetDate < Date.parse(response.headers.sunset)
-    ) {
-      set(response.headers, 'sunset', sunsetDate.toUTCString());
-    }
-    if (
-      response.headers.deprecation === undefined
-      || deprecationDate < Date.parse(response.headers.deprecation.slice(6, -1))
-    ) {
-      set(response.headers, 'deprecation', `date="${deprecationDate.toUTCString()}"`);
-    }
+    this.executor.updateDeprecationHeader(event, response);
   }
 }
 
