7b3a2ea5dc06338843fe76cee1f0f99f0a3865c9
Lukas Siemon <github@blackflux.com>
2020-08-26 11:05:02 -0700

    feat: added in-memory and s3 rate limiting

---
+++ src/util/limiter.js
@@ -0,0 +1,41 @@
+const crypto = require('crypto');
+const objectHash = require('object-hash-strict');
+const LRU = require('lru-cache-ext');
+
+const { ApiError } = require('../response');
+const Storage = require('./storage');
+
+const generateInterval = (identifier) => `${identifier}/${
+  new Date().toISOString().slice(0, 16).replace(/[-:T]/g, '/')
+}`;
+
+module.exports = ({ bucket, globalLimit, defaultRouteLimit }) => {
+  const memoryCache = new LRU({ maxAge: 60 * 1000 });
+  const storage = Storage(bucket);
+
+  return async ({
+    identifier,
+    route,
+    data,
+    routeLimit = defaultRouteLimit
+  }) => {
+    const interval = `${generateInterval(identifier)}/`;
+    const routeHash = objectHash(route);
+    const prefix = `${interval}${routeHash}/`;
+    if (memoryCache.has(interval) || memoryCache.has(prefix)) {
+      throw ApiError('Rate limit exceeded.', 429);
+    }
+    const [keys] = await Promise.all([
+      storage.list(interval),
+      storage.set(`${prefix}${crypto.randomBytes(16).toString('hex')}.json.gz`, data)
+    ]);
+    if (keys.length >= globalLimit) {
+      memoryCache.set(interval, true);
+      throw ApiError('Rate limit exceeded.', 429);
+    }
+    if (keys.filter((key) => key.startsWith(prefix)).length >= routeLimit) {
+      memoryCache.set(prefix, true);
+      throw ApiError('Rate limit exceeded.', 429);
+    }
+  };
+};
