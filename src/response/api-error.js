e4d9196ab6ad6d80da3e398e297f7bc7c7d1a2a2
Lukas Siemon <lukas.siemon@blackflux.com>
2022-04-01 14:09:25 -0700

    fix: adjusted error class name

--- src/response/api-error.js
+++ src/response/api-error.js
@@ -1,7 +1,7 @@
 export class ApiErrorClass extends Error {
   constructor(message, statusCode = 400, messageId = undefined, context = undefined) {
     super(message);
-    this.name = this.constructor.name;
+    this.name = 'ApiError';
     Error.captureStackTrace(this, this.constructor);
     this.statusCode = statusCode;
     this.headers = {};
