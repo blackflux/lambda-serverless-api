277f0105875bc7d9b2845d5cc2d5582cca3f96ad
Lukas Siemon <lukas.siemon@blackflux.com>
2023-07-17 12:08:47 -0700

    fix: updated deps
    BREAKING CHANGE: bumped min node version to v16

--- src/util/storage.js
+++ src/util/storage.js
@@ -1,9 +1,7 @@
 import assert from 'assert';
 import LRU from 'lru-cache-ext';
-import { logger } from 'lambda-monitor-logger';
-import AwsSdkWrap from 'aws-sdk-wrap';
 
-export default (S3, bucket) => {
+export default (awsSdkWrap, bucket) => {
   assert(bucket === null || typeof bucket === 'string');
   const memoryStorage = new LRU({
     ttl: 60 * 1000,
@@ -11,17 +9,12 @@ export default (S3, bucket) => {
   });
   const useMemory = bucket === null;
 
-  const { s3 } = AwsSdkWrap({
-    logger,
-    services: { S3 }
-  });
-
   return {
     set: async (key, data) => {
       if (useMemory) {
         memoryStorage.set(key, true);
       } else {
-        await s3.putGzipObject({
+        await awsSdkWrap.s3.putGzipObject({
           bucket,
           key,
           data: JSON.stringify(data)
@@ -37,7 +30,7 @@ export default (S3, bucket) => {
           }
         });
       } else {
-        const keys = await s3.listObjects({ bucket, prefix });
+        const keys = await awsSdkWrap.s3.listObjects({ bucket, prefix });
         keys.forEach(({ Key }) => {
           result.push(Key);
         });
